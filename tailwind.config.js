40ea82bf83c90b903ebe03dacbeff24ffcd049ae
CancerousSaintMichael <meineaccount@usa.com>
2023-04-13 19:39:58 +0000

    Update 3 files

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ export default {
         "very-pale-blue": "#f5f7ff",
         "desaturated-blue": "#7280a7",
         "dark-blue": "#1f2f56",
+        pattern: "#e2e8ff",
+      },
+      backgroundImage: {
+        "pattern-desktop": "url('/src/assets/pattern-background-desktop.svg')",
+        "pattern-mobile": "url('/src/assets/pattern-background-mobile.svg')",
       },
     },
   },
