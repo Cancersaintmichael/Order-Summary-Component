74d40814d94d4f01f6965c892af6bd5b1b190360
CancerousSaintMichael <meineaccount@usa.com>
2023-04-13 18:47:14 +0000

    Update index.html and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "pale-blue": "#f5f7ff",
+        "bright-blue": "#3829e0",
+        "very-pale-blue": "#f5f7ff",
+        "desaturated-blue": "#7280a7",
+        "dark-blue": "#1f2f56",
+      },
+    },
   },
   plugins: [],
 };
